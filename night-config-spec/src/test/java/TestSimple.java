733dc5d4f17d57ebeec51b294fd4100d2b3f1a5d
Taskeren <r0yalist@outlook.com>
2026-09-13 18:00:28 +0800

    Add more simple tests

--- night-config-spec/src/test/java/TestSimple.java
+++ night-config-spec/src/test/java/TestSimple.java
@@ -1,9 +1,11 @@
 import cn.elytra.nightconfig.Configuration;
+import com.electronwill.nightconfig.core.EnumGetMethod;
 import net.neoforged.neoforge.common.ModConfigSpec;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.file.Path;
+import java.time.format.TextStyle;
 import java.util.List;
 
 public class TestSimple {
@@ -22,11 +24,19 @@ public class TestSimple {
         ModConfigSpec.ConfigValue<List<? extends String>> baz = b
                 .comment("The BAZ list of String")
                 .defineList("baz", List.of("Hello", "Bonjour"), String::new, String.class::isInstance);
+        b.comment("This is a NESTED section?!").push("nest");
+        ModConfigSpec.EnumValue<TextStyle> nestedEnum = b
+                .comment("Some random enum?")
+                .defineEnum("enum", TextStyle.FULL, EnumGetMethod.ORDINAL_OR_NAME);
+        ModConfigSpec.IntValue nestedInt = b
+                .comment("Yet another Integer")
+                .defineInRange("int", 1, -1, 114514);
+        b.pop();
         ModConfigSpec spec = b.build();
 
         Configuration.openConfig(spec, Path.of("config", "1.toml"));
 
-        log.info("FOO {} BAR {} BAZ {}", foo.get(), bar.get(), String.join(",", baz.get()));
+        log.info("FOO {} BAR {} BAZ {} N_ENUM {} N_INT {}", foo.get(), bar.get(), String.join(",", baz.get()), nestedEnum.get().name(), nestedInt.get());
 
         while (true) {
             Thread.sleep(1000);
