49c102b8e6bdd79b69b99209305b13bd39b033ed
Taskeren <r0yalist@outlook.com>
2026-09-13 20:17:19 +0800

    Add contract to push

--- night-config-kotlin/src/main/kotlin/cn/elytra/nightconfig/BuilderExtensions.kt
+++ night-config-kotlin/src/main/kotlin/cn/elytra/nightconfig/BuilderExtensions.kt
@@ -1,11 +1,20 @@
+@file:OptIn(ExperimentalContracts::class)
+
 package cn.elytra.nightconfig
 
 import net.neoforged.neoforge.common.ModConfigSpec
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 
 inline fun ModConfigSpec.Builder.push(
     path: String,
     block: () -> Unit,
 ) {
+    contract {
+        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+    }
+
     push(path)
     block()
     pop()
